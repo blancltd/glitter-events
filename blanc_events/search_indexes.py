ebf0c45bd793cffb0fa6c554c8b448b028ce143b
Edvinas Jurevicius <ikonitas@gmail.com>
2015-11-10 15:18:33 +0000

    Add search indexes

---
+++ blanc_events/search_indexes.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from haystack import indexes
+
+from .models import Event
+
+
+class EventIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+
+    def get_model(self):
+        return Event
+
+    def index_queryset(self, using=None):
+        return self.get_model().objects.select_related().filter(
+            published=True
+        ).exclude(
+            current_version=None
+        )
