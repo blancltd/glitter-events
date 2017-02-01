8eaffba1ad869ea2011ff007d3936b366d0e3959
martinphellwig <martin.hellwig@gmail.com>
2017-02-01 13:28:22 +0000

    unbreak it

--- glitter_events/templatetags/events_tags.py
+++ glitter_events/templatetags/events_tags.py
@@ -13,11 +13,11 @@ def get_upcoming_events(count=5, category='', tags=''):
     event_list = Event.objects.published().filter(start__gt=timezone.now())
 
     # Optional filter by category
-    if not category.strip():
+    if category.strip():
         event_list = event_list.filter(category__slug=category)
 
     # Optional filter by tags
-    if not tags.strip(): 
+    if tags.strip(): 
         event_list = event_list.filter(tags__name__in=utils.parse_tags(tags))
 
     return event_list[:count]
