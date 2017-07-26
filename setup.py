ddef7ed09a8a374533ac7d8eebb1a19816c6131a
Alex Tomkins <tomkins@darkzone.net>
2017-07-26 10:40:39 +0100

    Version 0.1.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst', 'r', 'utf-8') as f:
 
 setup(
     name='django-glitter-events',
-    version='0.1.4',
+    version='0.1.5',
     description='Glitter Events for Django',
     long_description=readme,
     url='https://github.com/blancltd/django-glitter-events',
