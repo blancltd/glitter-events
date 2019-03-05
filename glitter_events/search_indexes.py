a485998447ffbe5a19ce8f9b49e61ac313c8241a
Edvinas Jurevicius <ikonitas@gmail.com>
2019-03-05 11:50:23 +0000

    Add option to ignore expired events for the search index

--- glitter_events/search_indexes.py
+++ glitter_events/search_indexes.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import datetime
+
+from django.conf import settings
+from django.utils import timezone
+
 from haystack import indexes
 
 from .models import Event
@@ -12,4 +17,11 @@ class EventIndex(indexes.SearchIndex, indexes.Indexable):
         return Event
 
     def index_queryset(self, using=None):
-        return self.get_model().objects.published().select_related()
+        if getattr(settings, 'GLITTER_EVENTS_SEARCH_INDEX_EXPIRED', None):
+            today = datetime.datetime.combine(date=datetime.date.today(), time=datetime.time.min)
+            today = timezone.make_aware(today)
+            qs = self.get_model().objects.filter(start__gte=today).select_related()
+
+        else:
+            qs = self.get_model().objects.published().select_related()
+        return qs
