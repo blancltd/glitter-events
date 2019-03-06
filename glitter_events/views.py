b4e90eab65a1bf6d2f0e3dc6901dd196f978386c
Richard Smith <richardsmith@dev.ngo>
2019-03-06 15:55:41 +0000

    better const name

--- glitter_events/views.py
+++ glitter_events/views.py
@@ -83,14 +83,14 @@ class EventListCategoryArchiveView(CategoryMixin, EventListArchiveView):
         the events with category__title='all_events'. The name of this type of
         category could change for each separate site so a settings variable has
         been introduced so it can be set on a per-site basis.
-        If the title of the given category matches the ALL_CATEGORIES_TITLE for
+        If the title of the given category matches the ALL_EVENTS_CATEGORY_TITLE for
         the site, all events will be returned, otherwise, the category filter
         will be applied as normal.
         """
         qs = super(EventListCategoryArchiveView, self).get_queryset()
         self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
-        if hasattr(settings, 'ALL_CATEGORIES_TITLE'):
-            if self.category.title == settings.ALL_CATEGORIES_TITLE:
+        if hasattr(settings, 'ALL_EVENTS_CATEGORY_TITLE'):
+            if self.category.title == settings.ALL_EVENTS_CATEGORY_TITLE:
                 return qs
         return qs.filter(category=self.category)
 
